0387a5ce2bf1f3b9f257425740efc784ac9e25d3
alem1r <alessandro.miragliotta.am@gmail.com>
2021-09-20 19:23:58 +0200

    final

--- src/avoider.cpp
+++ src/avoider.cpp
@@ -12,10 +12,7 @@
 
 ros::Publisher vel;
 bool cmdReceived = false;
-
 geometry_msgs::Twist vel_received;
-
-
 
 void star_cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
 { 
@@ -27,7 +24,6 @@ void laser_cmd_vel_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   if (!cmdReceived ) return; // per non processare un comando di velocità più volte
   cmdReceived = false;
-
 
   tf::TransformListener listener;
   laser_geometry::LaserProjection projector;
@@ -52,54 +48,53 @@ void laser_cmd_vel_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 Eigen::Isometry2f laser_transform = convertPose2D(transform_obstacle);
 Eigen::Vector2f p_min; // obstacle position rispetto al laser
 
-p_min(0) = cloud.points[540].x;
+p_min(0) = cloud.points[540].x; //la lunghezza di cloudpoints è 1080, la metà è 540 che sta davanti a me. (solo per inizializzare)
 p_min(1) = cloud.points[540].y;
 auto obstacle_pos = laser_transform * p_min;
 float obstacle_dist = sqrt(obstacle_pos(0)*obstacle_pos(0)+obstacle_pos(1)*obstacle_pos(1));
-auto obstacle_pos_actual = laser_transform * p_min;
 
-for(int i=cloud.points.size()/2-200;i<=cloud.points.size()/2+200;i++){
-Eigen::Vector2f p;
+const float min_detect = cloud.points.size()/2-200;
+const float max_detect = cloud.points.size()/2+200;
 
-p(0) = cloud.points[i].x; //la lunghezza di cloudpoints è 1080, la metà è 540 che sta davanti a me. cloud.points.size/2
-p(1) = cloud.points[i].y;  
+for(int i=min_detect;i<=max_detect;i++){
+  Eigen::Vector2f p;
 
-obstacle_pos_actual = laser_transform * p;
-float obstacle_dist_actual = sqrt(obstacle_pos_actual(0)*obstacle_pos_actual(0)+obstacle_pos_actual(1)*obstacle_pos_actual(1));
+  p(0) = cloud.points[i].x; 
+  p(1) = cloud.points[i].y;  
+
+  auto obstacle_pos_actual = laser_transform * p;
+  float obstacle_dist_actual = sqrt(obstacle_pos_actual(0)*obstacle_pos_actual(0)+obstacle_pos_actual(1)*obstacle_pos_actual(1));
 
   if(obstacle_dist_actual < obstacle_dist){
     obstacle_dist = obstacle_dist_actual;
     obstacle_pos = obstacle_pos_actual;
   }
-
-
-
 }
 
 if(obstacle_dist < 0.5 && vel_received.linear.x > 0 ){
-float force_intens = (1.0 / obstacle_dist) * 0.3 ;
-float force_x = -(obstacle_pos(0) / obstacle_dist) * force_intens;
-float force_y = -(obstacle_pos(1) / obstacle_dist) * force_intens;
+  float force_intens = (1.0 / obstacle_dist) * 0.3 ;
+  float force_x = -(obstacle_pos(0) / obstacle_dist) * force_intens;
+  float force_y = -(obstacle_pos(1) / obstacle_dist) * force_intens;
 
-geometry_msgs::Twist msg_send;
+  geometry_msgs::Twist msg_send;
 
-msg_send.linear.x = vel_received.linear.x + force_x;
-
-
-msg_send.linear.y = vel_received.linear.y + force_y;
-msg_send.linear.z = vel_received.linear.z;
-
-if(obstacle_pos_actual(1) > 0)
-  msg_send.angular.z =  - force_intens * 2; 
-else if (obstacle_pos_actual(1) < 0)
-  msg_send.angular.z =  force_intens * 2; 
-
-ROS_INFO("msg_send.linear.x %f",msg_send.linear.x);
+  msg_send.linear.x = vel_received.linear.x + force_x;
 
 
-vel.publish(msg_send);
+  msg_send.linear.y = vel_received.linear.y + force_y;
+  msg_send.linear.z = vel_received.linear.z;
 
-} else{
+  if(obstacle_pos(1) > 0)
+    msg_send.angular.z = - force_intens; 
+  else if (obstacle_pos(1) < 0)
+    msg_send.angular.z = force_intens; 
+
+  ROS_INFO("msg_send.linear.x %f",msg_send.linear.x);
+
+
+  vel.publish(msg_send);
+
+}else{
   vel.publish(vel_received);
 }
   
@@ -111,7 +106,6 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "collision_avoidance");
 
-  
   ros::NodeHandle n;
   //subscribe to cmd_vel topic
   ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel_call", 1, star_cmd_vel_callback);
